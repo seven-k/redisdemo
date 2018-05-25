cbd732d1a350aa17243955872b6c46e652a1b590
YinChanglei <425470936@qq.com>
2018-05-25 14:59:15 +0800

    id generated v1

--- src/main/java/com/ycl/redisdemo/controller/IDGenerator.java
+++ src/main/java/com/ycl/redisdemo/controller/IDGenerator.java
@@ -2,7 +2,7 @@ package com.ycl.redisdemo.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -24,7 +21,7 @@ public class IDGenerator {
     private static final long twepoch = Long.valueOf(year + "000000001");
 
     @Autowired
-    private RedisTemplate redisTemplate;
+    private StringRedisTemplate redisTemplate;
 
     /**
      * just for Test
@@ -34,26 +31,26 @@ public class IDGenerator {
         int i = (int) (Math.random() * 5);
         InetAddress localHost = InetAddress.getLocalHost();
         String hostName = localHost.getHostName();
-        return hostName + i;
+        return "hostName" + i;
 
     }
 
-    private String saveHostAndGenerateId(String hostName, int stepNum) {
+    private synchronized String saveHostAndGenerateId(String hostName, int stepNum) {
         String year = String.valueOf(LocalDateTime.now().getYear()).substring(2);
         long[] initValueArray = {
                 Long.valueOf(year + "000000001"), Long.valueOf(year + "000000002"), Long.valueOf(year + "000000003"),
                 Long.valueOf(year + "000000004"), Long.valueOf(year + "000000005"), Long.valueOf(year + "000000006"),
                 Long.valueOf(year + "000000007"), Long.valueOf(year + "000000008"), Long.valueOf(year + "000000009")
         };
-        ValueOperations operations = redisTemplate.opsForValue();
-        Set<String> keys = redisTemplate.keys("test:hostname:*");
+        ValueOperations<String, String> operations = redisTemplate.opsForValue();
+        Set<String> keys = redisTemplate.keys("test:hostname*");
         if (keys != null) {
             for (String key : keys) {
-                if (hostName.equals(key)) {  //已存在
+                if (("test:hostname:" + hostName).equals(key)) {  //已存在
                     String initValue = (String) operations.get(key);
                     long incrCount = operations.increment(hostName + "_incr_count", stepNum);
                     long param = Long.valueOf(initValue) + incrCount;
-                    System.out.println(hostName + ":" + param);
+                    System.out.println(param);
                     return "" + param;
                 }
 
@@ -62,15 +59,15 @@ public class IDGenerator {
             {  //不存在
                 long incrCount = operations.increment(hostName + "_incr_count", stepNum);
                 long param = initValueArray[hostCount] + incrCount;
-                operations.set("test:hostname:" + hostName, initValueArray[hostCount]);
-                System.out.println(hostName + ":" + param);
+                operations.set("test:hostname:" + hostName, String.valueOf(initValueArray[hostCount]));
+                System.out.println(param);
                 return "" + param;
             }
         } else {//第一个
             long incrCount = operations.increment(hostName + "_incr_count", stepNum);
             long param = initValueArray[0] + incrCount;
-            operations.set("test:hostname:" + hostName, initValueArray[0]);
-            System.out.println(hostName + ":" + param);
+            operations.set("test:hostname:" + hostName, String.valueOf(initValueArray[0]));
+            System.out.println(param);
             return "" + param;
         }
     }
@@ -82,7 +79,7 @@ public class IDGenerator {
      * @return
      */
     private String createId(int stepNum) {
-        ValueOperations operations = redisTemplate.opsForValue();
+        ValueOperations<String, String> operations = redisTemplate.opsForValue();
         long incrCount = operations.increment("incr_count", stepNum);
         long param = twepoch + incrCount;
         operations.set("incrCount", incrCount + "");
