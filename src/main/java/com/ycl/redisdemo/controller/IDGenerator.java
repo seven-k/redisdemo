70d1af7cdaa1befb246bcf0d2dcdd9c52449c919
YinChanglei <425470936@qq.com>
2018-05-29 18:30:53 +0800

    id generated v1

--- src/main/java/com/ycl/redisdemo/controller/IDGenerator.java
+++ src/main/java/com/ycl/redisdemo/controller/IDGenerator.java
@@ -1,6 +1,7 @@
 package com.ycl.redisdemo.controller;
 
 
+import org.apache.commons.lang.time.FastDateFormat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -28,24 +29,35 @@ public class IDGenerator {
         int i = (int) (Math.random() * 5);
         InetAddress localHost = InetAddress.getLocalHost();
         String hostName = localHost.getHostName();
-        return "hostName" + i;
+        return hostName + i;
 
     }
 
     private synchronized String saveHostAndGenerateId(String hostName, int stepNum) {
-        String year = String.valueOf(LocalDateTime.now().getYear()).substring(2);
-        long[] initValueArray = {
-                Long.valueOf(year + "000000001"), Long.valueOf(year + "000000002"), Long.valueOf(year + "000000003"),
-                Long.valueOf(year + "000000004"), Long.valueOf(year + "000000005"), Long.valueOf(year + "000000006"),
-                Long.valueOf(year + "000000007"), Long.valueOf(year + "000000008"), Long.valueOf(year + "000000009")
-        };
         ValueOperations<String, String> operations = redisTemplate.opsForValue();
-        Set<String> keys = redisTemplate.keys("test:hostname*");
+        String date = FastDateFormat.getInstance("yyMMddhhmm").format(new Date());
+        String timeTag = operations.get("adj_time_tag");
+        if (timeTag == null) {
+            operations.set("adj_time_tag", date);
+        } else {
+            if (!date.equals(timeTag)) {
+                operations.set("adj_time_tag", date);
+                Set<String> keys = redisTemplate.keys("adj:*");
+                for (String key : keys) redisTemplate.delete(key);
+            }
+        }
+        long[] initValueArray = {
+                Long.valueOf(date + "0000001"), Long.valueOf(date + "0000002"), Long.valueOf(date + "0000003"),
+                Long.valueOf(date + "0000004"), Long.valueOf(date + "0000005"), Long.valueOf(date + "0000006"),
+                Long.valueOf(date + "0000007"), Long.valueOf(date + "0000008"), Long.valueOf(date + "0000009"),
+                Long.valueOf(date + "0000010"), Long.valueOf(date + "0000011"), Long.valueOf(date + "0000012")
+        };
+        Set<String> keys = redisTemplate.keys("adj:hostname*");
         if (keys != null) {
             for (String key : keys) {
-                if (("test:hostname:" + hostName).equals(key)) {  //已存在
+                if (("adj:hostname:" + hostName).equals(key)) {  //已存在
                     String initValue = (String) operations.get(key);
-                    long incrCount = operations.increment(hostName + "_incr_count", stepNum);
+                    long incrCount = operations.increment("adj:"+hostName + ":incrCount", stepNum);
                     long param = Long.valueOf(initValue) + incrCount;
                     System.out.println(param);
                     return "" + param;
@@ -54,16 +66,16 @@ public class IDGenerator {
             }
             int hostCount = keys.size();
             {  //不存在
-                long incrCount = operations.increment(hostName + "_incr_count", stepNum);
+                long incrCount = operations.increment("adj:"+hostName + ":incrCount", stepNum);
                 long param = initValueArray[hostCount] + incrCount;
-                operations.set("test:hostname:" + hostName, String.valueOf(initValueArray[hostCount]));
+                operations.set("adj:hostname:" + hostName, String.valueOf(initValueArray[hostCount]));
                 System.out.println(param);
                 return "" + param;
             }
         } else {//第一个
-            long incrCount = operations.increment(hostName + "_incr_count", stepNum);
+            long incrCount = operations.increment("adj:"+hostName + ":incrCount", stepNum);
             long param = initValueArray[0] + incrCount;
-            operations.set("test:hostname:" + hostName, String.valueOf(initValueArray[0]));
+            operations.set("adj:hostname:" + hostName, String.valueOf(initValueArray[0]));
             System.out.println(param);
             return "" + param;
         }
@@ -72,7 +84,7 @@ public class IDGenerator {
 
     @GetMapping("/get")
     public void main() {
-        int taskSize = 5;
+        int taskSize = 30;
         // 创建一个线程池
         ExecutorService pool = Executors.newFixedThreadPool(taskSize);
         // 创建多个有返回值的任务
@@ -89,7 +101,7 @@ public class IDGenerator {
         public void run() {
             for (int i = 0; i < 1000; i++) {
                 try {
-                    saveHostAndGenerateId(getHostName(), 9);
+                    saveHostAndGenerateId(getHostName(), 12);
                 } catch (UnknownHostException e) {
                     e.printStackTrace();
                 }
